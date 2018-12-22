5bc1f4f31bdc8dd92969af5a7649cf951eee04d4
CoolONEOfficial <coolone.official@gmail.com>
2018-12-22 18:45:15 +0300

    Added markers parsing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -24,29 +25,45 @@ class MapsDemoState extends State<MapsDemo> {
       children: <Widget>[
         GoogleMap(
           onMapCreated: _onMapCreated,
+          options: GoogleMapOptions(
+            cameraPosition: CameraPosition(
+              target: LatLng(56.328619, 44.002833),
+              zoom: 17.0,
+              tilt: 30.0,
+            ),
+          ),
         ),
-        RaisedButton(
-          child: const Text('Go to London'),
-          onPressed: mapController == null
-              ? null
-              : () {
-                  mapController.animateCamera(CameraUpdate.newCameraPosition(
-                    const CameraPosition(
-                      bearing: 270.0,
-                      target: LatLng(51.5160895, -0.1294527),
-                      tilt: 30.0,
-                      zoom: 17.0,
-                    ),
-                  ));
-                },
-        ),
+//        RaisedButton(
+//          child: const Text('Go to London'),
+//          onPressed: mapController == null
+//              ? null
+//              : () {
+//                  mapController.animateCamera(CameraUpdate.newCameraPosition(
+//                    const CameraPosition(
+//                      bearing: 270.0,
+//                      target: LatLng(56.328619, 44.002833),
+//                      tilt: 30.0,
+//                      zoom: 17.0,
+//                    ),
+//                  ));
+//                },
+//        ),
       ],
     );
   }
 
   void _onMapCreated(GoogleMapController controller) {
+    Firestore.instance.collection('tasks').snapshots().listen((snapshot) {
+      for (var mDoc in snapshot.documents) {
+        var mDocGeoPoint = mDoc.data["coord"] as GeoPoint;
+        mapController.addMarker(MarkerOptions(
+            consumeTapEvents: true,
+            position: LatLng(mDocGeoPoint.latitude, mDocGeoPoint.longitude)));
+      }
+    });
     setState(() {
       mapController = controller;
+      mapController.onMarkerTapped.add((marker) {});
     });
   }
 }
